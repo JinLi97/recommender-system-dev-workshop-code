ca85699880129c1c25cd9e7e26f9e236de115eb7
JinLi97 <li.jin5@northeastern.edu>
2021-09-03 07:33:49 +0000

    for deploy

--- src/offline/lambda/package_code_to_s3.sh
+++ src/offline/lambda/package_code_to_s3.sh
@@ -6,7 +6,12 @@ pwd
 
 Stage=$1
 if [[ -z $Stage ]];then
-  Stage='dev-workshop'
+  Stage='dev'
+fi
+
+METHOD=$2
+if [[ -z $METHOD ]];then
+  METHOD='customize'
 fi
 
 echo "Stage=$Stage"
@@ -20,7 +25,7 @@ if [[ -n $AWS_DEFAULT_REGION ]];then
 fi
 
 if [[ -z $REGION ]]; then
-  REGION='ap-northeast-1'
+  REGION='ap-southeast-1'
 fi
 
 echo "AWS_CMD: $AWS_CMD"
@@ -36,6 +41,11 @@ PREFIX=ops-data
 
 echo "BUCKET_BUILD=${BUCKET_BUILD}"
 echo "Create S3 Bucket: ${BUCKET_BUILD} if not exist"
+
+#$AWS_CMD s3api --region $REGION create-bucket --bucket ${BUCKET_BUILD}  \
+#--create-bucket-configuration LocationConstraint=$REGION || true
+
+$AWS_CMD s3api --region $REGION create-bucket --bucket ${BUCKET_BUILD}  > /dev/null 2>&1 || true
 
 lambda_funcs=(
   precheck-lambda
@@ -69,8 +79,35 @@ for lambda_func in ${lambda_funcs[@]}; do
   rm ./${lambda_func}.py
 done
 
-rm -rf package
+if [[ "$METHOD" != "customize"  && "$METHOD" != "" ]]
+then
+  ps_lambda_funcs=(
+    create-dataset-import-job-lambda
+    check-dataset-import-job-status-lambda
+    update-solution-version-lambda
+    check-solution-version-status-lambda
+    update-campaign-lambda
+    check-campaign-status-lambda
+    create-batch-inference-job-lambda
+    check-batch-inference-job-status-lambda
+    sync-solution-version-lambda
+  )
+  for lambda_func in ${ps_lambda_funcs[@]}; do
+    cp ../ps-lambda/${lambda_func}.py .
+    cd package
+    zip -r ../${lambda_func}.zip . >/dev/null
+    cd ..
+    zip -g ${lambda_func}.zip ./${lambda_func}.py
 
+    if [[ $? -ne 0 ]]; then
+      echo "error!!!"
+      exit 1
+    fi
+    rm ./${lambda_func}.py
+  done
+fi
+
+rm -rf package
 
 $AWS_CMD s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/code/lambda/
 if [[ $? -ne 0 ]]; then
